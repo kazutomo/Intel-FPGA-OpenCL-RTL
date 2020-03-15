2ed486fa55999e94bbd6e11e06a1a812d232348c
Kazutomo Yoshii <kazutomo@mcs.anl.gov>
2020-03-15 20:16:24 +0000

    compiled for de10nano. tested on de10nano

--- buildlib.sh
+++ buildlib.sh
@@ -4,7 +4,7 @@ rm -f rev8.aoco cnt32.aoco libtest.aoclib
 aocl library hdl-comp-pkg rev8.xml  -o rev8.aoco
 aocl library hdl-comp-pkg cnt32.xml -o cnt32.aoco
 
-aocl library create -name testlib -vendor kzt -version 16.0 -o libtest.aoclib  rev8.aoco cnt32.aoco
+aocl library create -name testlib -vendor kzt -version 18.1 -o libtest.aoclib  rev8.aoco cnt32.aoco
 
-time aoc -v  -fast-compile --report -l libtest.aoclib testbitlib.cl 2>&1 | tee aoc.log
+time aoc -v --report -l libtest.aoclib testbitlib.cl 2>&1 | tee aoc.log
 
